9116e4a77d615bac3b4a536de4a27fa319b1058f
dax <88696221+Dax233@users.noreply.github.com>
2024-06-17 09:49:42 +0800

    add. trigonometric_functions

--- bridgeDesign/temp.py
+++ bridgeDesign/temp.py
@@ -1,23 +1,3 @@
-def convert_sqrt(expression):
-    while '√' in expression:
-        start_index = expression.find('√') + 1
-        # Initialize the parentheses counter
-        parentheses_count = 0
-        for i in range(start_index, len(expression)):
-            if expression[i] == '(':
-                parentheses_count += 1
-            elif expression[i] == ')':
-                parentheses_count -= 1
-            # When the counter is zero, we found the matching closing parenthesis
-            if parentheses_count == 0:
-                # Insert the power of (1/2) at the right position
-                expression = (expression[:start_index-1] + '(' + expression[start_index:i+1] + 
-                              ')**(1/2)' + expression[i+1:])
-                break
-    return expression
-
-# Example usage:
-original_expression = '√((a+b)+c)'
-converted_expression = convert_sqrt(original_expression)
-print('Original:', original_expression)
-print('Converted:', converted_expression)
+import numpy as np
+a = 45
+print (np.sin(a*np.pi/180))
--- bridgeDesign/wordFormulaCalculator.py
+++ bridgeDesign/wordFormulaCalculator.py
@@ -7,7 +7,9 @@
 
 import re
 import pyperclip
+import numpy as np
 from colorama import init, Fore
+
 
 # 输出预设
 init()
@@ -17,6 +19,30 @@ warning = Fore.YELLOW + '[!]' + Fore.RESET
 debug = Fore.YELLOW + '[!debug]' + Fore.RESET
 done = Fore.CYAN + '[>]' + Fore.RESET
 separator = '\n================================\n'
+
+def trigonometric_functions(expression):
+    while '\"sin\"' in expression:
+        start_index = expression.find('\"sin\" ') + 6
+        for i in range(start_index,len(expression)):
+            if expression[i] == '∘' and expression[i-2:i] == '**' :
+                print(np.sin(int(expression[start_index:i-2])*np.pi/180))
+                expression = (expression[:start_index-6] + str(np.sin(int(expression[start_index:i-2])*np.pi/180)) + expression[i+1:])
+                break
+    while '\"cos\"' in expression:
+        start_index = expression.find('\"cos\" ') + 6
+        for i in range(start_index,len(expression)):
+            if expression[i] == '∘' and expression[i-2:i] == '**' :
+                print(np.sin(int(expression[start_index:i-2])*np.pi/180))
+                expression = (expression[:start_index-6] + str(np.cos(int(expression[start_index:i-2])*np.pi/180)) + expression[i+1:])
+                break
+    while '\"tan\"' in expression:
+        start_index = expression.find('\"tan\" ') + 6
+        for i in range(start_index,len(expression)):
+            if expression[i] == '∘' and expression[i-2:i] == '**' :
+                print(np.sin(int(expression[start_index:i-2])*np.pi/180))
+                expression = (expression[:start_index-6] + str(np.tan(int(expression[start_index:i-2])*np.pi/180)) + expression[i+1:])
+                break
+    return expression
 
 def convert_sqrt(expression):
     while '√' in expression:
@@ -31,8 +57,7 @@ def convert_sqrt(expression):
             # When the counter is zero, we found the matching closing parenthesis
             if parentheses_count == 0:
                 # Insert the power of (1/2) at the right position
-                expression = (expression[:start_index-1] + '(' + expression[start_index:i+1] + 
-                              ')**(1/2)' + expression[i+1:])
+                expression = (expression[:start_index-1] + '(' + expression[start_index:i+1] + ')**(1/2)' + expression[i+1:])
                 break
     return expression
 
@@ -61,6 +86,8 @@ def convert_expression(expression: str) -> str:
     """转换万恶的根号符号"""
     expression = convert_sqrt(expression)
     expression = convert_sqrt_4(expression)
+    """算三角函数"""
+    expression = trigonometric_functions(expression)
     # expression = re.sub(r'∜\((.*?)\)', r'(\1)**(1/4)', expression)
     # expression = re.sub(r'√\((.*?)\)', r'(\1)**(1/2)', expression)
     expression = re.sub(r'(\d|\))(\()', r'\1*\2', expression)
